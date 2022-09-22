e46a339e24c4ce5516da6aa75c23aa1b2d9c940b
moorthijg <95556836+moorthijg@users.noreply.github.com>
2022-09-22 21:48:15 +0530

    Add files via upload

---
+++ GETTINGINPUT.py
@@ -0,0 +1,14 @@
+# getting input
+from itertools import product
+
+
+name = input("enter your name")
+print("my name is",name)
+# giving input 
+
+
+#get product of two numbers
+num1 = int(input("enter your value"))
+num2 = int(input("enter another value"))
+product = num1 * num2
+print("the product is ", product)
